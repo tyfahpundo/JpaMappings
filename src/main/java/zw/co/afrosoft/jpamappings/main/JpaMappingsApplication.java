44e62ab1f3dc5229ef25ee6863a8d94728b947dc
tyfah <tafadzwaallentyfah@gmail.com>
2021-07-25 09:36:12 +0200

    first commit

---
+++ src/main/java/zw/co/afrosoft/jpamappings/main/JpaMappingsApplication.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft.jpamappings.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMappingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMappingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zw/co/afrosoft/jpamappings/JpaMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft.jpamappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMappingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
